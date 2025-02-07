96049fef08b12c036c5604587c20b41416ed79c2
Elaine Jiang <ejiang0207@mit.edu>
2025-02-06 23:24:33 -0500

    edit resume section

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -5,19 +5,22 @@ const Resume = () => {
   return (
     <div className="resume-container">
       <h2 id="resume">resume</h2>
+      <span class="bg-left-bottom bg-gradient-to-r from-blue-300 to-pink-200 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out hover:text-blue-300">
+        Email me for a resume
+      </span>
 
-      <a
+      {/* <a
         href="https://drive.google.com/uc?export=download&id=1BcOLoet7PfjilQlyvl7hUyJc_Rgz3aES"
         download="ElaineJiang_Resume.pdf"
         className="group transition-all duration-300 ease-in-out"
       >
-        Email me for a resume.
-        {/* <button className="py-2 mt-1 mb-1 rounded text-black">
+        
+        <button className="py-2 mt-1 mb-1 rounded text-black">
           <span class="bg-left-bottom bg-gradient-to-r from-blue-300 to-pink-200 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out hover:text-blue-300">
             Download Resume
           </span>
-        </button> */}
-      </a>
+        </button>
+      </a> */}
       {/* <iframe
         src="https://drive.google.com/file/d/1BcOLoet7PfjilQlyvl7hUyJc_Rgz3aES/preview"
         width="100%"
